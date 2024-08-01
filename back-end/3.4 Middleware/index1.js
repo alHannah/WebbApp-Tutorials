7da2abd9c84c4d8de7087934cdb1a7c8aec7ae7a
hannahsim <111653900+hannah442003@users.noreply.github.com>
2024-08-01 22:25:15 +0800

    middleware

--- back-end/3.4 Middleware/index1.js
+++ back-end/3.4 Middleware/index1.js
@@ -2,9 +2,17 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+//  To create a body for URL-encoded request like form submission
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (request, respond) => {
+  console.log(request.body);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
